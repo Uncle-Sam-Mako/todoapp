ecea8ce28c96ddec4d60778bda939ce2c3017a70
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2022-08-12 23:19:44 +0200

    Add TodoList table

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
+from email.policy import default
 import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #Our flask app instance
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://uncle-sam:1234@localhost:5432/todoApp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 #Our database instance
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  todos = db.relationship('Todo', backref='list', lazy=True)
+
 
 # get form request
 # @app.route('/todos/create', methods=['POST'])
@@ -85,6 +99,10 @@ def deleteTask(todo_id):
 db.create_all()
 
 #Rendering function HTML
+@app.route('/list/<list_id>')
+def get_list_id(list_id):
+  return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.order_by('id').all())
+  return redirect(url_for('get_list_id', list_id=1))
